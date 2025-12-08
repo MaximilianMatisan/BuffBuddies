0e2524e7007acba80fe66bf3e97458ed845ed3c0
Maki919 <zneaz22@gmail.com>
2025-12-08 19:11:46 +0100

    added tab-buttons

--- src/client/app.rs
+++ src/client/app.rs
@@ -1,11 +1,14 @@
+use crate::client::bb_tab::tab::Tab;
 use crate::client::mascots::Mascot;
 
 pub struct App {
+    pub screen: Tab,
     pub active_mascot: Mascot,
 }
 impl Default for App {
     fn default() -> Self {
         App {
+            screen: Tab::Home,
             active_mascot: Mascot::Duck,
         }
     }
