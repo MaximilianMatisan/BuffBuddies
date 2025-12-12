a378779da4ebe619d1bffef9ff22f35b8aa992b5
Stefano Petrina <trinaamp2@icloud.com>
2025-12-12 22:13:58 +0100

    Added new font and apply it to required components

--- src/client/bb_theme/text_format.rs
+++ src/client/bb_theme/text_format.rs
@@ -2,12 +2,22 @@ use crate::client::bb_theme::color;
 use iced::Renderer;
 use iced_core::widget::Text;
 use iced_core::{Font, Theme};
+use iced_core::font::Family::Name;
+use iced_core::font::Stretch::Normal;
+use iced_core::font::Style;
+use iced_core::font::Weight::ExtraBold;
 
+pub const FIRA_SANS_EXTRABOLD: Font = Font{
+    family:Name("Fira Sans"),
+    weight: ExtraBold,
+    stretch: Normal,
+    style: Style::Normal
+};
 pub const ARIAL_ROUNDED_B: Font = Font::with_name("Arial Rounded MT Bold");
 
 pub fn format_button_text(msg: Text<'_, Theme, Renderer>) -> Text<'_, Theme, Renderer>{
-    msg.font(ARIAL_ROUNDED_B).color(color::TEXT_COLOR).center()
+    msg.font(FIRA_SANS_EXTRABOLD).color(color::TEXT_COLOR).center()
 }
 pub fn format_description_text(msg: Text<'_, Theme, Renderer>) -> Text<'_, Theme, Renderer> {
-    msg.font(ARIAL_ROUNDED_B).color(color::DESCRIPTION_TEXT_COLOR).size(15)
+    msg.font(FIRA_SANS_EXTRABOLD).color(color::DESCRIPTION_TEXT_COLOR).size(15)
 }
