76a18a2d919f6538f7894af919d0cc9b5b88a27a
Maximilian Matisan <maximilian.matisan@gmail.com>
2025-12-15 15:10:57 +0100

    Changed weight representation in StrengthSet

--- src/client/backend/exercise/set.rs
+++ src/client/backend/exercise/set.rs
@@ -1,17 +1,18 @@
 use crate::client::backend::exercise::weight::{ExerciseWeight, Kg};
 
 pub struct StrengthSet {
-    pub(crate) weight: ExerciseWeight,
+    pub(crate) weight: Kg,
     pub(crate) reps: u64
 }
 impl StrengthSet {
     pub fn new(weight: ExerciseWeight, reps: u64) -> Self {
+        let kg = weight.to_kg();
         StrengthSet {
-            weight,
+            weight: kg,
             reps
         }
     }
     pub fn total_lifted_weight(&self) -> Kg {
-        self.weight.to_kg() * self.reps as Kg
+        self.weight * self.reps as Kg
     }
 }
