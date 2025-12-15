1c341b35e1d0c8bfad51067124592b3fcc105329
Maximilian Matisan <maximilian.matisan@gmail.com>
2025-12-15 14:36:05 +0100

    Added basic exercise/set/weight hierarchy

---
+++ src/client/backend/exercise/weight.rs
@@ -0,0 +1,29 @@
+use std::cmp::Ordering;
+
+pub type Kg = f64;
+
+pub enum ExerciseWeight {
+    Bodyweight,
+    BodyweightPlusKg(Kg),
+    Kg(Kg)
+}
+impl PartialEq for ExerciseWeight {
+    fn eq(&self, other: &Self) -> bool {
+        self.to_kg() == other.to_kg()
+    }
+}
+impl PartialOrd for ExerciseWeight {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.to_kg().partial_cmp(&other.to_kg())
+    }
+}
+impl ExerciseWeight {
+    pub fn to_kg(&self) -> Kg {
+        let user_weight = 0.0; //TODO get user weight
+        match self {
+            ExerciseWeight::Bodyweight => user_weight,
+            ExerciseWeight::BodyweightPlusKg(kg) => user_weight + kg,
+            ExerciseWeight::Kg(kg) => *kg
+        }
+    }
+}
