e3a51edd24567b97c854249152e1d8b08b17acf9
Maximilian Matisan <maximilian.matisan@gmail.com>
2025-12-16 14:03:37 +0100

    Outsourced data from ProgressWidget to App

--- src/client/gui/app.rs
+++ src/client/gui/app.rs
@@ -1,4 +1,5 @@
 use crate::client::backend::login_state::LoginState;
+use crate::client::backend::exercise::exercise::Exercise;
 use crate::client::gui::bb_tab::tab::Tab;
 use crate::client::gui::bb_widget::activity::activity::ActivityWidget;
 use crate::client::gui::mascots::Mascot;
@@ -9,7 +10,11 @@ pub struct App {
     pub active_mascot: Mascot,
     pub activity_widget: ActivityWidget,
     pub login_state: LoginState,
+
+    pub exercises: Vec<Exercise>,
+    pub selected_exercise_id: usize
 }
+
 impl Default for App {
     fn default() -> Self {
         let default_mascot = Mascot::default();
@@ -17,8 +22,19 @@ impl Default for App {
             loading: false,
             screen: Tab::Home,
             active_mascot: default_mascot.clone(),
-            activity_widget: ActivityWidget::new(default_mascot),
             login_state: LoginState::default(),
+            activity_widget: ActivityWidget::new(default_mascot.clone()),
+            
+            exercises: vec![Exercise::new("Benchpress".to_string()), Exercise::new("Lateral pulldown".to_string())],
+            selected_exercise_id: 0
         }
+    }
+}
+impl App {
+    pub fn get_selected_exercise(&self) -> Option<&Exercise> {
+        if self.selected_exercise_id >= self.exercises.len() {
+            return None
+        }
+        Some(&self.exercises[self.selected_exercise_id])
     }
 }
--- src/client/gui/bb_widget.rs
+++ src/client/gui/bb_widget.rs
@@ -2,3 +2,4 @@ pub mod workout_preset;
 pub mod widget_utils;
 pub mod shop;
 pub mod activity;
+pub mod progress;
