4de2754fccb4b3ea1a4f51070a2658ac63843691
Maximilian Matisan <maximilian.matisan@gmail.com>
2026-01-11 16:16:02 +0100

    Moved `combo_box` styling into `bb_theme::combo_box`

--- src/client/gui/bb_tab/settings.rs
+++ src/client/gui/bb_tab/settings.rs
@@ -1,9 +1,15 @@
 use crate::client::gui::user_interface::{Message, UserInterface};
 use iced::Element;
-use iced::widget::Row;
+use iced::widget::{image};
+use iced_core::image::Handle;
+use crate::client::gui::size;
 
 impl UserInterface {
     pub fn settings_screen(&self) -> Element<Message> {
-        Row::new().into()
+        let profile_picture = image(Handle::from_path(self.app.user_manager.user_information.profile_picture_handle.clone()))
+            .width(size::LARGE_PROFILE_PICTURE_DIMENSION)
+            .height(size::LARGE_PROFILE_PICTURE_DIMENSION);
+
+        profile_picture.into()
     }
 }
--- src/client/gui/bb_theme.rs
+++ src/client/gui/bb_theme.rs
@@ -2,3 +2,4 @@ pub mod color;
 pub mod container;
 pub mod custom_button;
 pub mod text_format;
+pub mod combo_box;
