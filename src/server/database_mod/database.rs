ee8be10f18f4f8c417663fe829e60c224757750d
robert989 <roberthenneke@gmail.com>
2026-01-13 21:55:02 +0100

    added coinbalance, gender, workoutstreak, user description and friendship database. Also added function to update said variables and function to add friend

--- src/server/database_mod/database.rs
+++ src/server/database_mod/database.rs
@@ -17,9 +17,13 @@ pub async fn init_db(pool: &SqlitePool) -> Result<(), sqlx::Error> {
     username TEXT NOT NULL UNIQUE,
     user_password TEXT NOT NULL,
     weekly_workout_goal INTEGER NOT NULL,
-    weight INTEGER NOT NULL,
-    height INTEGER NOT NULL,
-    profile_picture TEXT
+    weekly_workout_streak INTEGER NOT NULL,
+    coin_balance INTEGER NOT NULL,
+    weight FLOAT NOT NULL,
+    height FLOAT NOT NULL,
+    gender TEXT NOT NULL,
+    profile_picture TEXT,
+    description TEXT
     );",
     )
     .execute(pool)
@@ -97,7 +101,20 @@ pub async fn init_db(pool: &SqlitePool) -> Result<(), sqlx::Error> {
 
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (preset_id) REFERENCES preset (id)
-    )",
+    );",
+    )
+    .execute(pool)
+    .await?;
+
+    sqlx::query(
+        "CREATE TABLE IF NOT EXISTS friendship (
+        username TEXT NOT NULL,
+        friendname TEXT NOT NULL,
+        PRIMARY KEY (username, friendname),
+
+        FOREIGN KEY (username) REFERENCES users(username),
+        FOREIGN KEY (friendname) REFERENCES users(username)
+    );",
     )
     .execute(pool)
     .await?;
@@ -110,14 +127,18 @@ pub async fn add_user(
     pool: &SqlitePool,
     username: &str,
     password: &str,
+    gender: &str,
 ) -> Result<(), sqlx::Error> {
-    sqlx::query("INSERT INTO users (username, user_password, weekly_workout_goal, weight, height, profile_picture)
-         VALUES (?, ?, ?, ?, ?, ?)")
+    sqlx::query("INSERT INTO users (username, user_password, weekly_workout_goal, weekly_workout_streak, coin_balance, weight, height, gender, profile_picture)
+         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)")
         .bind(username)
         .bind(password)
         .bind(0)
         .bind(0)
         .bind(0)
+        .bind(0)
+        .bind(0)
+        .bind(gender)
         .bind("")
         .execute(pool)
         .await?;
@@ -208,4 +229,59 @@ pub async fn get_user_id(pool: &SqlitePool, name: &str) -> Result<i64, sqlx::Err
         .fetch_optional(pool)
         .await?;
     Ok(user_id.expect("getting id failed ").get("id"))
+}
+#[allow(dead_code)]
+pub async fn update_user_gender(
+    pool: &SqlitePool,
+    username: &str,
+    new_gender: &str,
+) -> Result<(), sqlx::Error> {
+    sqlx::query("UPDATE users SET gender = ? WHERE username = ?")
+        .bind(new_gender)
+        .bind(username)
+        .execute(pool)
+        .await?;
+    Ok(())
+}
+#[allow(dead_code)]
+pub async fn update_user_weekly_workout_streak(
+    pool: &SqlitePool,
+    username: &str,
+    new_streak: i32,
+) -> Result<(), sqlx::Error> {
+    sqlx::query("UPDATE users SET weekly_workout_streak = ? WHERE username = ?")
+        .bind(new_streak)
+        .bind(username)
+        .execute(pool)
+        .await?;
+    Ok(())
+}
+#[allow(dead_code)]
+pub async fn update_user_coin_balance(
+    pool: &SqlitePool,
+    username: &str,
+    new_balance: i32,
+) -> Result<(), sqlx::Error> {
+    sqlx::query("UPDATE users SET coin_balance = ? WHERE username = ?")
+        .bind(new_balance)
+        .bind(username)
+        .execute(pool)
+        .await?;
+    Ok(())
+}
+#[allow(dead_code)]
+pub async fn add_friend(
+    pool: &SqlitePool,
+    username: &str,
+    friendname: &str,
+) -> Result<(), sqlx::Error> {
+    if username != friendname {
+        sqlx::query("INSERT OR IGNORE INTO friendship (username, friendname) VALUES (?,?)")
+            .bind(username)
+            .bind(friendname)
+            .execute(pool)
+            .await?;
+    }
+
+    Ok(())
 }
