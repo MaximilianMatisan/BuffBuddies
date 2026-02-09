a6f023e99f4697cce388f8e2c6ba9d6d837262a7
Stefano Petrina <trinaamp2@icloud.com>
2026-02-10 00:39:06 +0100

    Introduced animation for circle

--- src/client/gui/bb_widget/circle_widget.rs
+++ src/client/gui/bb_widget/circle_widget.rs
@@ -1,6 +1,7 @@
+use std::time::Duration;
 use iced::alignment;
 use iced::mouse;
-use iced::widget::canvas::{Cache, Geometry, LineCap, Path, Stroke, stroke, LineJoin, LineDash};
+use iced::widget::canvas::{Cache, Geometry, LineCap, Path, Stroke, stroke, LineJoin, LineDash, event};
 use iced::widget::{canvas, container, text};
 use iced::{
     Degrees, Element, Fill, Font, Radians, Rectangle, Renderer, Size, Subscription, Theme,
@@ -8,41 +9,104 @@ use iced::{
 };
 use iced::advanced::graphics::text::cosmic_text::Shaping;
 use iced::widget::canvas::path::Arc;
+use iced_anim::{Animated, Animation, Event, Motion};
 use iced_core::{color, renderer, Point};
 use iced_core::alignment::{Horizontal, Vertical};
+use crate::client::backend::exercise_manager::ExerciseManager;
+use crate::client::gui::app::App;
 use crate::client::gui::bb_theme::color::CONTAINER_COLOR;
 use crate::client::gui::bb_theme::text_format::FIRA_SANS_EXTRABOLD;
-use crate::client::gui::bb_widget::progress::ProgressWidget;
 use crate::client::gui::user_interface::Message;
+use crate::common::mascot_mod::mascot::Mascot;
+use crate::common::mascot_mod::mascot_trait::MascotTrait;
 
 const CIRCLE_WIDGET_WIDTH: f32 = 300.0;
 const CIRCLE_WIDGET_HEIGHT: f32 = 300.0;
 
-pub struct CircleWidget {
-    width: f32,
-    height: f32,
-    circle: Cache
+
+pub struct CircleWidget<'a> {
+    active_mascot: Mascot,
+    exercise_manager: &'a ExerciseManager,
+    circle_widget_state: &'a CircleWidgetState,
 }
 
-impl CircleWidget {
+#[derive(Clone, Debug)]
+pub enum  CircleMessage{
+    UpdateAnimation(Event<f32>)
+}
+
+impl<'a> CircleWidget<'a> {
+    pub(crate) fn new(app: &'a App) -> Self {
+        CircleWidget {
+            active_mascot: app.mascot_manager.selected_mascot,
+            exercise_manager: &app.exercise_manager,
+            circle_widget_state: &app.circle_widget_state,
+        }
+    }
+    pub(crate) fn view (self) -> Element<'a, Message> {
+
+        let draw_percentage = &self.circle_widget_state.animation_progress;
+
+        let canvas = canvas(self).width(CIRCLE_WIDGET_WIDTH).height(CIRCLE_WIDGET_HEIGHT);
+
+        Animation::new(draw_percentage, canvas)
+            .on_update(|event| Message::Circle(CircleMessage::UpdateAnimation(event)))
+            .into()
+
+    }
+}
+pub struct CircleWidgetState {
+    width: f32,
+    height: f32,
+    circle: Cache,
+    pub animation_progress: Animated<f32>,
+}
+
+impl CircleWidgetState {
+    pub(crate) fn update_circle(&self) {
+        self.circle.clear();
+
+    }
+}
+
+impl CircleWidgetState {
     pub fn new() -> Self {
+
+        let animation_motion = Motion {
+            response: Duration::from_millis(3000),
+            damping: Motion::SMOOTH.damping(),
+        };
+
         Self {
             width: CIRCLE_WIDGET_WIDTH,
             height:CIRCLE_WIDGET_HEIGHT,
-            circle: Cache::default()
+            circle: Cache::default(),
+            animation_progress: Animated::new(0.0, animation_motion)
         }
-    }
-
-
-    pub(crate) fn view(self) -> Element<'static, Message> {
-        let canvas = canvas(self).width(CIRCLE_WIDGET_WIDTH).height(CIRCLE_WIDGET_HEIGHT);
-
-        container(canvas).padding(20).into()
     }
 }
 
-impl<Message> canvas::Program<Message> for CircleWidget {
+impl canvas::Program<Message> for CircleWidget<'_> {
     type State = ();
+
+    fn update(
+        &self,
+        _state: &mut Self::State,
+        event: iced::widget::canvas::Event,
+        _bounds: Rectangle,
+        _cursor: iced_core::mouse::Cursor,
+    ) -> (iced::event::Status, std::option::Option<Message>) {
+        self.circle_widget_state.update_circle();
+
+        match event {
+
+            _ => (event::Status::Ignored,
+                Some(crate::client::gui::user_interface::Message::Circle(CircleMessage::UpdateAnimation(
+                    iced_anim::Event::Target(1.0)
+
+            ))))
+        }
+    }
 
     fn draw(
         &self,
@@ -54,7 +118,7 @@ impl<Message> canvas::Program<Message> for CircleWidget {
     ) -> Vec<Geometry> {
         use chrono::Timelike;
 
-        let circle_widget = self.circle.draw(renderer, bounds.size(), |frame| {
+        let circle_widget = self.circle_widget_state.circle.draw(renderer, bounds.size(), |frame| {
 
 
             let mut center = frame.center();
@@ -78,13 +142,13 @@ impl<Message> canvas::Program<Message> for CircleWidget {
                     center,
                     radius,
                     start_angle: Degrees(0.0 - 90.0).into(),
-                    end_angle: Degrees(80.0 - 90.0).into(),
+                    end_angle: Degrees(80.0 * self.circle_widget_state.animation_progress.value() - 90.0).into(),
                 });
             }), Stroke {
                 width: 20.0,
                 line_cap: LineCap::Round,
                 line_join: LineJoin::Round,
-                style: stroke::Style::Solid(color!(255, 165, 0)),
+                style: stroke::Style::Solid(self.active_mascot.get_primary_color()),
                 line_dash: Default::default(),
             });
 
@@ -96,8 +160,8 @@ impl<Message> canvas::Program<Message> for CircleWidget {
                 builder.arc(Arc {
                     center,
                     radius,
-                    start_angle: Degrees(110.0 - 90.0 ).into(),
-                    end_angle: Degrees(330.0 - 90.0 ).into(),
+                    start_angle: Degrees(110.0 * self.circle_widget_state.animation_progress.value() - 90.0 ).into(),
+                    end_angle: Degrees(330.0  - 90.0).into(),
                 });
             }), Stroke {
                 width: 20.0,
