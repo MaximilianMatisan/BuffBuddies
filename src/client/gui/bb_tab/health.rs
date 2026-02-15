27b73cb9c0e29310c6565d8cf0be8f3ee768ba42
Stefano Petrina <trinaamp2@icloud.com>
2026-02-15 16:05:18 +0100

    Initialized ProgressBarWidget

--- src/client/gui/bb_tab/health.rs
+++ src/client/gui/bb_tab/health.rs
@@ -3,14 +3,17 @@ use crate::client::gui::bb_widget::bmi_calculator;
 use crate::client::gui::bb_widget::widget_utils::LARGE_INDENT;
 use crate::client::gui::user_interface::Message;
 use iced::Element;
-use iced::widget::Column;
+use iced::widget::{progress_bar, Column};
+use crate::client::gui::bb_widget::progress_bar::ProgressBarWidget;
 
 impl App {
     pub fn health_screen(&self) -> Element<Message> {
         let bmi_widget = bmi_calculator::BMIWidget::new(self).view();
+        let progress_bar = ProgressBarWidget::new(self).view();
 
         let content = Column::new()
             .push(bmi_widget)
+            .push(progress_bar)
             .padding(LARGE_INDENT)
             .spacing(LARGE_INDENT)
             .into();
