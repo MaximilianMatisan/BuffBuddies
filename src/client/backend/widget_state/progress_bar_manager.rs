48fd9cef23e9d331ef44509a76ecac92eda12746
Stefano Petrina <trinaamp2@icloud.com>
2026-02-20 13:48:19 +0100

    Enabled save/discard functionality in health tab

--- src/client/backend/widget_state/progress_bar_manager.rs
+++ src/client/backend/widget_state/progress_bar_manager.rs
@@ -4,16 +4,31 @@ pub struct ProgressBarStateManager {
     pub water_progress_bar_state: ProgressBarState,
     pub steps_progress_bar_state: ProgressBarState,
     pub sleep_progress_bar_state: ProgressBarState,
-    pub edit_mode: bool
 }
 
 impl ProgressBarStateManager {
     pub(crate) fn new() -> Self {
-        Self{
+        Self {
             water_progress_bar_state: ProgressBarState::new(1.0, 3.0),
-            steps_progress_bar_state: ProgressBarState::new(2000.0,10000.0),
-            sleep_progress_bar_state: ProgressBarState::new(7.0,8.0),
-            edit_mode: false
+            steps_progress_bar_state: ProgressBarState::new(2000.0, 10000.0),
+            sleep_progress_bar_state: ProgressBarState::new(7.0, 8.0),
+        }
+    }
+
+    pub(crate) fn copy_states(progress_bar_state: &ProgressBarStateManager) -> Self {
+        Self {
+            water_progress_bar_state: ProgressBarState::new(
+                progress_bar_state.water_progress_bar_state.current_value,
+                progress_bar_state.water_progress_bar_state.goal_value,
+            ),
+            steps_progress_bar_state: ProgressBarState::new(
+                progress_bar_state.steps_progress_bar_state.current_value,
+                progress_bar_state.steps_progress_bar_state.goal_value,
+            ),
+            sleep_progress_bar_state: ProgressBarState::new(
+                progress_bar_state.sleep_progress_bar_state.current_value,
+                progress_bar_state.sleep_progress_bar_state.goal_value,
+            ),
         }
     }
 }
