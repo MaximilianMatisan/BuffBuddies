4b73adc40c5b68f515cde850cfb10a1bc4f0f7de
Stefano Petrina <trinaamp2@icloud.com>
2026-02-26 14:02:42 +0100

    Added animation to new widget buttons in workout tab

--- src/client/gui/bb_widget/new_widget.rs
+++ src/client/gui/bb_widget/new_widget.rs
@@ -2,20 +2,24 @@ use crate::client::gui::bb_tab::tab::Tab;
 use crate::client::gui::bb_theme::color::{
     CONTAINER_COLOR, HIGHLIGHTED_CONTAINER_COLOR, LIGHTER_CONTAINER_COLOR,
 };
-use crate::client::gui::bb_theme::custom_button::create_button_style;
+use crate::client::gui::bb_theme::custom_button::{create_button_style, create_gradient_mascot_style};
 use crate::client::gui::bb_theme::text_format::format_button_text;
 use crate::client::gui::bb_widget::workout::{
     DEFAULT_RECENT_WORKOUT_WIDGET_HEIGHT, DEFAULT_WORKOUT_PRESET_WIDGET_HEIGHT,
     DEFAULT_WORKOUT_WIDGET_WIDTH,
 };
 use crate::client::gui::user_interface::Message;
+use crate::common::mascot_mod::mascot::Mascot;
+use crate::common::mascot_mod::mascot_trait::MascotTrait;
 use iced::Renderer;
-use iced::widget::column;
 use iced::widget::{Button, text};
+use iced::widget::{Column, column};
 use iced::widget::{Space, button};
+use iced_anim::Motion;
 use iced_core::alignment::Horizontal;
 use iced_core::border::Radius;
 use iced_core::{Border, Length, Theme};
+use std::time::Duration;
 
 const SCALE: f32 = 0.9;
 const BUTTONS_WIDTH: f32 = DEFAULT_WORKOUT_WIDGET_WIDTH;
@@ -36,51 +40,54 @@ const BORDER: Border = Border {
     },
 };
 
-pub fn new_workout_widget_button<'a>() -> Button<'a, Message, Theme, Renderer> {
-    button(
-        column!(
-            Space::with_height(Length::Fill),
-            format_button_text(text("New workout")).size(DEFAULT_TITLE_FONT_SIZE),
-            format_button_text(text("+").size(ADD_SYMBOL_SIZE)),
-            Space::with_height(Length::Fill)
-        )
+pub fn new_workout_widget_button<'a>(
+    mascot: Mascot,
+) -> iced_anim::widget::Button<'a, Message, Theme, Renderer> {
+    let content = Column::new()
+        .push(Space::with_height(Length::Fill))
+        .push(format_button_text(text("New workout")).size(DEFAULT_TITLE_FONT_SIZE))
+        .push(format_button_text(text("+").size(ADD_SYMBOL_SIZE)))
+        .push(Space::with_height(Length::Fill))
         .width(BUTTONS_WIDTH)
         .height(NEW_WORKOUT_BUTTON_HEIGHT)
-        .align_x(Horizontal::Center),
-    )
-    .style(|_, status| {
-        create_button_style(
-            status,
-            BORDER,
-            HIGHLIGHTED_CONTAINER_COLOR,
-            LIGHTER_CONTAINER_COLOR,
-            CONTAINER_COLOR,
-        )
-    })
-    .width(BUTTONS_WIDTH)
-    .height(NEW_WORKOUT_BUTTON_HEIGHT)
-    .on_press(Message::Select(Tab::CreateWorkout))
+        .align_x(Horizontal::Center);
+
+    iced_anim::widget::button(content)
+        .animation(Motion {
+            response: Duration::from_millis(300),
+            damping: Motion::SMOOTH.damping(),
+        })
+        .style(move |_, status| {
+            create_gradient_mascot_style(
+                status,
+                mascot
+            )
+        })
+        .width(BUTTONS_WIDTH)
+        .height(NEW_WORKOUT_BUTTON_HEIGHT)
+        .on_press(Message::Select(Tab::CreateWorkout))
 }
 
-pub fn new_preset_widget_button<'a>() -> Button<'a, Message, Theme, Renderer> {
-    button(
-        column!(
-            Space::with_height(Length::Fill),
-            format_button_text(text("New preset")).size(DEFAULT_TITLE_FONT_SIZE),
-            format_button_text(text("+").size(ADD_SYMBOL_SIZE)),
-            Space::with_height(Length::Fill)
-        )
+pub fn new_preset_widget_button<'a>(mascot: Mascot) -> iced_anim::widget::Button<'a, Message, Theme, Renderer> {
+
+    let content = Column::new()
+        .push(Space::with_height(Length::Fill))
+        .push(format_button_text(text("New preset")).size(DEFAULT_TITLE_FONT_SIZE))
+        .push(format_button_text(text("+").size(ADD_SYMBOL_SIZE)))
+        .push(Space::with_height(Length::Fill))
         .width(BUTTONS_WIDTH)
         .height(NEW_PRESET_BUTTON_HEIGHT)
-        .align_x(Horizontal::Center),
-    )
-    .style(|_, status| {
-        create_button_style(
+        .align_x(Horizontal::Center);
+
+    iced_anim::widget::button(content)
+        .animation(Motion {
+            damping: Motion::SMOOTH.damping,
+            response: Duration::from_millis(350),
+        })
+    .style(move |_, status| {
+        create_gradient_mascot_style(
             status,
-            BORDER,
-            HIGHLIGHTED_CONTAINER_COLOR,
-            LIGHTER_CONTAINER_COLOR,
-            CONTAINER_COLOR,
+            mascot
         )
     })
     .width(BUTTONS_WIDTH)
