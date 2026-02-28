0d2d311e7e15f54598219b963978097207404987
felix <felix.m.mitterer@icloud.com>
2026-02-28 17:03:29 +0100

    added tests for login_state

--- src/client/backend/login_state.rs
+++ src/client/backend/login_state.rs
@@ -1,5 +1,6 @@
 use crate::client::server_communication::server_communicator::LoginRequest;
 
+#[derive(Debug, PartialEq)]
 pub enum LoginStateError {
     UsernameEmpty,
     PasswordEmpty,
@@ -37,5 +38,62 @@ impl LoginState {
             return Err(LoginStateError::PasswordEmpty);
         }
         Ok((self.username.clone(), self.password.clone()).into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn to_error_message_password_empty() {
+        let login_state_error = LoginStateError::PasswordEmpty;
+        assert_eq!(
+            login_state_error.to_error_message(),
+            "Password can't be empty!".to_string()
+        );
+    }
+    #[test]
+    fn to_error_message_username_empty() {
+        let login_state_error = LoginStateError::UsernameEmpty;
+        assert_eq!(
+            login_state_error.to_error_message(),
+            "Username can't be empty!".to_string()
+        );
+    }
+    #[test]
+    fn try_login_no_username() {
+        let login_state = LoginState {
+            username: "".to_string(),
+            password: "123".to_string(),
+            state: Default::default(),
+            error_text: "".to_string(),
+        };
+        assert_eq!(login_state.try_login(), Err(LoginStateError::UsernameEmpty));
+    }
+    #[test]
+    fn try_login_no_password() {
+        let login_state = LoginState {
+            username: "123".to_string(),
+            password: "".to_string(),
+            state: Default::default(),
+            error_text: "".to_string(),
+        };
+        assert_eq!(login_state.try_login(), Err(LoginStateError::PasswordEmpty));
+    }
+
+    #[test]
+    fn try_login_all_filled() {
+        let login_state = LoginState {
+            username: "123".to_string(),
+            password: "1234".to_string(),
+            state: Default::default(),
+            error_text: "".to_string(),
+        };
+        let login_request = LoginRequest {
+            username: "123".to_string(),
+            password: "1234".to_string(),
+        };
+        assert_eq!(login_state.try_login(), Ok(login_request));
     }
 }
