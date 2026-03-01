a3fae58826f8860d00206e329142a4f0134329d9
Stefano Petrina <trinaamp2@icloud.com>
2026-03-01 17:58:18 +0100

    Moved graph logic from graph.rs to graph_logic.rs

--- src/client/gui/bb_widget/chart_widget/graph.rs
+++ src/client/gui/bb_widget/chart_widget/graph.rs
@@ -37,10 +37,11 @@ use iced_core::keyboard::Key;
 use iced_core::mouse::Cursor;
 use iced_core::{Length, Point, Rectangle, Size, Theme};
 use crate::client::gui::bb_widget::canvas_utils::{draw_line, generate_dashed_stroke, generate_stroke};
+use crate::client::gui::bb_widget::chart_widget::graph_logic::{calculate_points, chop_dates, chop_weights, extract_dates, extract_weights};
 
-const GRAPH_PADDING: f32 = 50.0;
+pub(crate) const GRAPH_PADDING: f32 = 50.0;
 const AXIS_FONT_SIZE: f32 = 12.0;
-const FREQUENCY_OF_Y_AXIS_LABELS: u32 = 12;
+pub(crate) const FREQUENCY_OF_Y_AXIS_LABELS: u32 = 12;
 const AXIS_THICKNESS: f32 = 5.0;
 pub const MAX_AMOUNT_POINTS: u8 = 40;
 pub const MAX_X_LABELS: u8 = 11;
@@ -135,7 +136,7 @@ pub struct GraphWidgetState {
     visible_points: bool,
     visible_cursor_information: bool,
     visible_vertical_lines: bool,
-    points_to_draw: u8,
+    pub(crate) points_to_draw: u8,
     pub shown_chart_type: ChartTypes,
 }
 
@@ -762,119 +763,6 @@ fn draw_axis_labels(
     });
 }
 
-
-fn chop_weights(graph_widget_state: &GraphWidgetState, y_values: Vec<Kg>) -> Vec<Kg> {
-    let chop_start_index: isize =
-        y_values.len() as isize - graph_widget_state.points_to_draw as isize;
-
-    let safe_chop_start_index = match chop_start_index {
-        ..=0 => 0,
-        _ => chop_start_index,
-    };
-
-    y_values[(safe_chop_start_index) as usize..].to_vec()
-}
-
-fn chop_dates(graph_widget_state: &GraphWidgetState, x_values: Vec<NaiveDate>) -> Vec<NaiveDate> {
-    let chop_start_index: isize =
-        x_values.len() as isize - graph_widget_state.points_to_draw as isize;
-
-    let safe_chop_start_index = match chop_start_index {
-        ..=0 => 0,
-        _ => chop_start_index,
-    };
-
-    x_values[(safe_chop_start_index) as usize..].to_vec()
-}
-
-fn calculate_points(graph_widget_state: &GraphWidgetState, y_values: Vec<Kg>) -> Vec<Point> {
-    let chopped_y_values = &chop_weights(graph_widget_state, y_values.clone());
-
-    //PADDING LEFT AND RIGHT: LEFT FOR Y_LABELS, RIGHT FOR FREE SPACE
-    let block_width =
-        (CHART_WIDGET_WIDTH - GRAPH_PADDING * 2.0) / graph_widget_state.points_to_draw as f32; //division with 0 is not possible since limit is 1
-    //PADDING UP AND DOWN:  UP FOR Y-AXIS-ARROW SPACE,DOWN FOR X-LABELS
-    let block_height =
-        (CHART_WIDGET_HEIGHT - GRAPH_PADDING * 2.0) / FREQUENCY_OF_Y_AXIS_LABELS as f32;
-    let mut current_x = CHART_WIDGET_WIDTH - GRAPH_PADDING;
-
-    //I can unwrap() since the function is not going to get called if exercises.len() = 0
-    let min_y: Kg = (chopped_y_values
-        .iter()
-        .map(|value| (*value * 10.0) as usize)
-        .min()
-        .unwrap_or(0)) as f32
-        / 10.0; //TODO: USE FUNCTION IN weight.rs when connecting to ExerciseManager
-    let max_y: Kg = (chopped_y_values
-        .iter()
-        .map(|value| (*value * 10.0) as usize)
-        .max()
-        .unwrap_or(0)) as f32
-        / 10.0; //TODO: USE FUNCTION IN weight.rs when connecting to ExerciseManager
-    let delta = max_y - min_y;
-    let percentage = |current_y: Kg| {
-        if delta == 0.0 {
-            1.0
-        } else {
-            (current_y - min_y) / delta
-        }
-    };
-
-    let height_padding_for_arrow = block_height;
-    let x_axis_padding = block_height;
-    let height_graph: f32 =
-        CHART_WIDGET_HEIGHT - GRAPH_PADDING * 2.0 - height_padding_for_arrow - x_axis_padding;
-    let lowest_point_graph: f32 = CHART_WIDGET_HEIGHT - GRAPH_PADDING - x_axis_padding;
-
-    //FORMULA: lowest_point-(y_min - current_y)/(max_y - min_y) * height_graph
-    let calculate_graph_value =
-        |y: &Kg| -> f32 { lowest_point_graph - (percentage(*y) * height_graph) };
-
-    let mut new_y_values = vec![];
-
-    let mut x_values = vec![];
-
-    let mut points = vec![];
-
-    //CALCULATE Y-VALUES
-    for y_value in chopped_y_values {
-        new_y_values.push(calculate_graph_value(y_value));
-    }
-
-    //CALCULATE X-VALUES
-    for _x_value in 1..=graph_widget_state.points_to_draw {
-        x_values.push(current_x);
-        current_x -= block_width
-    }
-    x_values.reverse();
-
-    //ZIP X- AND Y-VALUES
-    for i in 0..chopped_y_values.len() {
-        points.push(Point {
-            x: x_values[i],
-            y: new_y_values[i],
-        })
-    }
-
-    points
-}
-
-fn extract_weights(exercise_data_points: &ExerciseDataPoints) -> Vec<Kg> {
-    let mut weights: Vec<Kg> = vec![];
-    for (_date, weight) in exercise_data_points {
-        weights.push(*weight)
-    }
-    weights
-}
-
-fn extract_dates(exercise_data_points: &ExerciseDataPoints) -> Vec<NaiveDate> {
-    let mut dates: Vec<NaiveDate> = vec![];
-    for (date, _weight) in exercise_data_points {
-        dates.push(*date)
-    }
-
-    dates
-}
 
 impl canvas::Program<Message> for GraphWidget<'_> {
     type State = GraphWidgetState; //not used yet
---
+++ src/client/gui/bb_widget/chart_widget/graph_logic.rs
@@ -0,0 +1,119 @@
+use chrono::NaiveDate;
+use iced_core::Point;
+use crate::client::gui::bb_widget::chart_widget::chart::{CHART_WIDGET_HEIGHT, CHART_WIDGET_WIDTH};
+use crate::client::gui::bb_widget::chart_widget::graph::GraphWidgetState;
+use crate::common::exercise_mod::exercise::ExerciseDataPoints;
+use crate::common::exercise_mod::weight::Kg;
+
+pub fn chop_weights(graph_widget_state: &GraphWidgetState, y_values: Vec<Kg>) -> Vec<Kg> {
+    let chop_start_index: isize =
+        y_values.len() as isize - graph_widget_state.points_to_draw as isize;
+
+    let safe_chop_start_index = match chop_start_index {
+        ..=0 => 0,
+        _ => chop_start_index,
+    };
+
+    y_values[(safe_chop_start_index) as usize..].to_vec()
+}
+
+pub fn chop_dates(graph_widget_state: &GraphWidgetState, x_values: Vec<NaiveDate>) -> Vec<NaiveDate> {
+    let chop_start_index: isize =
+        x_values.len() as isize - graph_widget_state.points_to_draw as isize;
+
+    let safe_chop_start_index = match chop_start_index {
+        ..=0 => 0,
+        _ => chop_start_index,
+    };
+
+    x_values[(safe_chop_start_index) as usize..].to_vec()
+}
+
+pub fn calculate_points(graph_widget_state: &GraphWidgetState, y_values: Vec<Kg>) -> Vec<Point> {
+    let chopped_y_values = &chop_weights(graph_widget_state, y_values.clone());
+
+    //PADDING LEFT AND RIGHT: LEFT FOR Y_LABELS, RIGHT FOR FREE SPACE
+    let block_width =
+        (CHART_WIDGET_WIDTH - crate::client::gui::bb_widget::chart_widget::graph::GRAPH_PADDING * 2.0) / graph_widget_state.points_to_draw as f32; //division with 0 is not possible since limit is 1
+    //PADDING UP AND DOWN:  UP FOR Y-AXIS-ARROW SPACE,DOWN FOR X-LABELS
+    let block_height =
+        (CHART_WIDGET_HEIGHT - crate::client::gui::bb_widget::chart_widget::graph::GRAPH_PADDING * 2.0) / crate::client::gui::bb_widget::chart_widget::graph::FREQUENCY_OF_Y_AXIS_LABELS as f32;
+    let mut current_x = CHART_WIDGET_WIDTH - crate::client::gui::bb_widget::chart_widget::graph::GRAPH_PADDING;
+
+    //I can unwrap() since the function is not going to get called if exercises.len() = 0
+    let min_y: Kg = (chopped_y_values
+        .iter()
+        .map(|value| (*value * 10.0) as usize)
+        .min()
+        .unwrap_or(0)) as f32
+        / 10.0; //TODO: USE FUNCTION IN weight.rs when connecting to ExerciseManager
+    let max_y: Kg = (chopped_y_values
+        .iter()
+        .map(|value| (*value * 10.0) as usize)
+        .max()
+        .unwrap_or(0)) as f32
+        / 10.0; //TODO: USE FUNCTION IN weight.rs when connecting to ExerciseManager
+    let delta = max_y - min_y;
+    let percentage = |current_y: Kg| {
+        if delta == 0.0 {
+            1.0
+        } else {
+            (current_y - min_y) / delta
+        }
+    };
+
+    let height_padding_for_arrow = block_height;
+    let x_axis_padding = block_height;
+    let height_graph: f32 =
+        CHART_WIDGET_HEIGHT - crate::client::gui::bb_widget::chart_widget::graph::GRAPH_PADDING * 2.0 - height_padding_for_arrow - x_axis_padding;
+    let lowest_point_graph: f32 = CHART_WIDGET_HEIGHT - crate::client::gui::bb_widget::chart_widget::graph::GRAPH_PADDING - x_axis_padding;
+
+    //FORMULA: lowest_point-(y_min - current_y)/(max_y - min_y) * height_graph
+    let calculate_graph_value =
+        |y: &Kg| -> f32 { lowest_point_graph - (percentage(*y) * height_graph) };
+
+    let mut new_y_values = vec![];
+
+    let mut x_values = vec![];
+
+    let mut points = vec![];
+
+    //CALCULATE Y-VALUES
+    for y_value in chopped_y_values {
+        new_y_values.push(calculate_graph_value(y_value));
+    }
+
+    //CALCULATE X-VALUES
+    for _x_value in 1..=graph_widget_state.points_to_draw {
+        x_values.push(current_x);
+        current_x -= block_width
+    }
+    x_values.reverse();
+
+    //ZIP X- AND Y-VALUES
+    for i in 0..chopped_y_values.len() {
+        points.push(Point {
+            x: x_values[i],
+            y: new_y_values[i],
+        })
+    }
+
+    points
+}
+
+pub fn extract_weights(exercise_data_points: &ExerciseDataPoints) -> Vec<Kg> {
+    let mut weights: Vec<Kg> = vec![];
+    for (_date, weight) in exercise_data_points {
+        weights.push(*weight)
+    }
+    weights
+}
+
+pub fn extract_dates(exercise_data_points: &ExerciseDataPoints) -> Vec<NaiveDate> {
+    let mut dates: Vec<NaiveDate> = vec![];
+    for (date, _weight) in exercise_data_points {
+        dates.push(*date)
+    }
+
+    dates
+}
