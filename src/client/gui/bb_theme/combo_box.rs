dd9f780b3c9411bc63fa84fa82ac29ca0634ebb6
Maximilian Matisan <maximilian.matisan@gmail.com>
2026-03-13 09:40:01 +0100

    Added shadow to `combo_box` menu style

--- src/client/gui/bb_theme/combo_box.rs
+++ src/client/gui/bb_theme/combo_box.rs
@@ -1,4 +1,5 @@
 use crate::client::gui::bb_theme::color;
+use crate::client::gui::bb_theme::color::DARK_SHADOW;
 use crate::client::gui::bb_theme::container::DEFAULT_CONTAINER_RADIUS;
 use crate::client::gui::bb_theme::custom_button::DEFAULT_BUTTON_RADIUS;
 use crate::common::exercise_mod::exercise::Exercise;
@@ -7,7 +8,7 @@ use crate::common::mascot_mod::mascot_trait::MascotTrait;
 use iced::overlay::menu;
 use iced::widget::text_input::Status;
 use iced::widget::{combo_box, text_input};
-use iced_core::{Background, Border, Color, Theme};
+use iced_core::{Background, Border, Color, Shadow, Theme, Vector};
 
 /// Styling of the iced text input widget (also used by combo_box)
 pub fn create_text_input_style(
@@ -40,7 +41,14 @@ pub fn create_menu_style(mascot: &Mascot) -> impl Fn(&Theme) -> menu::Style {
         text_color: color::TEXT_COLOR,
         selected_text_color: color::TEXT_COLOR,
         selected_background: Background::Color(mascot.get_primary_color()),
-        shadow: Default::default(),
+        shadow: Shadow {
+            color: Color {
+                a: 0.3,
+                ..DARK_SHADOW
+            },
+            offset: Vector::new(0.0, 0.0),
+            blur_radius: 15.0,
+        },
     }
 }
 pub fn get_combo_box_all_exercises_state(exercise: &[Exercise]) -> combo_box::State<String> {
