87e271327b2e32d97de0685826609c5916dd5302
Maximilian Matisan <maximilian.matisan@gmail.com>
2026-03-13 18:10:06 +0100

    Replaced the main function with two separate binaries

---
+++ src/bin/client.rs
@@ -0,0 +1,5 @@
+use buff_buddies::client::gui::user_interface::client_main;
+
+fn main() -> iced::Result {
+    client_main()
+}
--- src/main.rs
+++
@@ -1,5 +0,0 @@
-use buff_buddies::run;
-pub fn main() -> iced::Result {
-    //TODO replace with 2 separate binaries in the future
-    run()
-}
