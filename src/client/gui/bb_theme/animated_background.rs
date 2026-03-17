cd57d82d01523cadb129f45db65c4fe66276c28a
Maximilian Matisan <maximilian.matisan@gmail.com>
2026-03-17 18:14:01 +0100

    Added util for random start point of background animation

--- src/client/gui/bb_theme/animated_background.rs
+++ src/client/gui/bb_theme/animated_background.rs
@@ -1,19 +1,18 @@
 use crate::client::backend::widget_state::widget_state_manager::WidgetMessage;
-use crate::client::backend::widget_state::widget_state_manager::WidgetMessage::Circle;
 use crate::client::gui::app::App;
 use crate::client::gui::bb_theme::container::{ContainerStyle, create_container_style};
-use crate::client::gui::bb_widget::circle_widget::CircleMessage;
 use crate::client::gui::user_interface::Message;
 use crate::client::gui::user_interface::Message::Widget;
 use crate::common::mascot_mod::mascot::Mascot;
 use crate::common::mascot_mod::mascot_trait::MascotTrait;
-use iced::widget::canvas::{Cache, Geometry, Path, Stroke, Style};
+use iced::widget::canvas::{Cache, Frame, Geometry, Path, Stroke, Style};
 use iced::widget::{Action, canvas, container};
-use iced::{Element, Renderer};
+use iced::{Element, Renderer, Task};
 use iced_anim::{Animated, Animation, Event, Motion};
 use iced_core::mouse::Cursor;
-use iced_core::{Length, Rectangle, Theme};
+use iced_core::{Length, Point, Rectangle, Size, Theme};
 use std::time::Duration;
+use rand::RngExt;
 
 pub struct BackgroundAnimation<'a> {
     state: &'a BackgroundAnimationState,
@@ -113,6 +112,20 @@ impl<'a> canvas::Program<Message> for BackgroundAnimation<'a> {
 pub enum BackgroundAnimationMessage {
     UpdateAnimation(Event<f32>),
 }
+impl BackgroundAnimationMessage {
+    pub fn update(self, app: &mut App) -> Task<Message> {
+        match self {
+            BackgroundAnimationMessage::UpdateAnimation(event) => {
+                app.widget_manager
+                    .background_animation_state
+                    .animation_progress
+                    .update(event);
+                app.widget_manager.background_animation_state.cache.clear();
+            }
+        }
+        Task::none()
+    }
+}
 
 pub fn animated_line_background(app: &App) -> Element<'_, Message> {
     let animated_line: Element<Message> = BackgroundAnimation::new(app).view();
@@ -126,4 +139,27 @@ pub fn animated_line_background(app: &App) -> Element<'_, Message> {
             None,
         ))
         .into()
+}
+
+/// Returns a random point on an edge of the frame
+fn get_random_start_point_of_line(size: Size) -> Point {
+    let mut rng = rand::rng();
+
+    // Whether the Point should start on a horizontal or vertical line
+    let horizontal_start = rng.random_bool(0.5);
+
+    // Whether the Point should start on the
+    //  * top(first) or bottom(second)
+    //  * left(first) or right(second)
+    // edge, interpretation depends on `horizontal_start`
+    let first_edge = rng.random_bool(0.5);
+
+    let offset_on_edge: f32 = rng.random_range(0.0..=1.0);
+
+    match (horizontal_start, first_edge) {
+        (true, true) => Point::new(size.width * offset_on_edge, 0.0), // Top
+        (true, false) => Point::new(size.width * offset_on_edge, size.height), // Bottom
+        (false, true) => Point::new(0.0, size.height * offset_on_edge), // Left
+        (false, false) => Point::new(size.width, size.height * offset_on_edge), // Right
+    }
 }
